1baa48c246dbb9005f85a89027f9362315547968
SpliffRa <spliffraida@gmail.com>
2026-09-21 14:15:53 +0300

    chore: bump versionCode=30, versionName=1.4.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.jackdaw.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.4.4"
+        versionCode = 30
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
